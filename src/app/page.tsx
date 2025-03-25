df0cdc78a2946d31bec50f7a64f9a56316336dbd
hara <haraprasad45@gmail.com>
2025-03-25 13:30:52 +0530

    about us page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutUsSection from "@/components/sections/home/about-section";
 import HeroSection from "@/components/sections/home/hero-section";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
+      <AboutUsSection />
     </main>
   );
 }
---
+++ src/components/sections/home/about-section.tsx
@@ -0,0 +1,5 @@
+import TeamSection from "@/components/team";
+
+export default function AboutUsSection() {
+  return <TeamSection />;
+}
