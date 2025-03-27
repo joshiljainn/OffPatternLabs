42f39072a59c69f2d8043991f07fd993d7460ceb
hara <haraprasad45@gmail.com>
2025-03-28 01:57:58 +0530

    projects section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutUsSection from "@/components/sections/home/about-section";
 import HeroSection from "@/components/sections/home/hero-section";
+import PortfolioSection from "@/components/sections/home/portfolio-section";
 import StatsSection from "@/components/sections/home/stats";
 import Testimonials from "@/components/testimonials";
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
+      <PortfolioSection />
       <AboutUsSection />
       <StatsSection />
       <Testimonials />
---
+++ src/components/portfolio-card.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { CustomCursorElement } from "./custom-cursor-element";
+import { ScrollView } from "./scroll-view";
+
+export default function PortfolioCard({
+  card,
+}: {
+  card: {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+  };
+}) {
+  return (
+    <div>
+      <CustomCursorElement
+        cursor={<div className="text-zinc-950 text-lg font-medium">View</div>}
+      >
+        <ScrollView>
+          <div className="group hover:scale-105 transition-all duration-500 ">
+            <a href={card.link} target="_blank" rel="noreferrer">
+              <img
+                className=" w-full grayscale-25 hover:grayscale-0 rounded-md object-cover object-top  transition-all duration-500  "
+                src={card.image}
+                alt={card.title}
+              />
+              <div className="mt-4">
+                <h3 className="text-title text-base font-medium ">
+                  {card.title}
+                </h3>
+                <p className="text-muted-foreground text-sm">
+                  {card.description}
+                </p>
+              </div>
+            </a>
+          </div>
+        </ScrollView>
+      </CustomCursorElement>
+    </div>
+  );
+}
