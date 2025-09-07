6644bac367d0c99757b7252c8e0de415bc7f00c1
Joshil Jain <joshiljain@Joshils-MacBook-Air.local>
2025-09-07 21:14:08 +0530

    Fix: Add asset prefix for correct pathing

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   basePath: '/OffPatternLabs',
+  assetPrefix: '/OffPatternLabs',
   images: {
     remotePatterns: [
       {
