88de7aa2c23cd27e0d6d7bc2f0312aa9a34f99a3
Joshil Jain <joshiljain@Joshils-MacBook-Air.local>
2025-09-08 15:58:12 +0530

    Revert "Revert "Remove unused full-version page""
    This reverts commit f694c0781ee521df4145435f413ab9bfa6836735.

--- src/app/full-version/page.tsx
+++
@@ -1,9 +0,0 @@
-import FullVersionSection from "@/components/sections/home/full-version-section";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <FullVersionSection />
-    </main>
-  );
-}
